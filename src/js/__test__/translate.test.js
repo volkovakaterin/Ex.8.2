e3c7dc691d671496d1023e71907978de18ebab3d
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-03-21 18:08:24 +0400

    tests changed

--- src/js/__test__/translate.test.js
+++ src/js/__test__/translate.test.js
@@ -2,15 +2,11 @@
 import ErrorRepository from '../map';
 
 test('translate', () => {
-  expect(() => {
-    const map = new ErrorRepository();
-    map.translate(5);
-  }).toBe('Unknown error');
+  const map = new ErrorRepository();
+  expect(map.translate(5)).toBe('Unknown error');
 });
 
 test('translate1', () => {
-  expect(() => {
-    const map = new ErrorRepository();
-    map.translate(2);
-  }).toBe('Другая ошибка');
+  const map = new ErrorRepository();
+  expect(map.translate(2)).toBe('Другая ошибка');
 });
